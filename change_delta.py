09ac3b50403513ce721fb6ccb71ff018b54cc8a5
Tom Arnfeld <tarnfeld@cloudflare.com>
2017-04-20 17:36:50 +0200

    Added script to calculate serial deltas

---
+++ change_delta.py
@@ -0,0 +1,31 @@
+
+import sys
+import json
+from collections import defaultdict
+from collections import Counter
+
+dst_serials = defaultdict(dict)
+dst_serials_counts = defaultdict(Counter)
+
+for line in sys.stdin:
+    blob = json.loads(line.rstrip())
+    if 'result' not in blob or 'answers' not in blob['result']:
+        continue
+
+    if 'lts' not in blob or blob['lts'] > 100:
+        continue
+
+    dst = blob['dst_addr']
+    serial = blob['result']['answers'][0]['SERIAL']
+
+    dst_serials[serial][dst] = dst_serials[serial].get(dst, blob['timestamp'])
+    dst_serials_counts[serial][dst] += 1
+
+for serial in dst_serials.keys():
+    serial_first_appeared = min(dst_serials[serial].values())
+
+    for dst, dst_first in dst_serials[serial].items():
+        delta = dst_first - serial_first_appeared
+        frequency_per_dst = dst_serials_counts[serial][dst]
+        if frequency_per_dst > 1000:
+            print ",".join([str(v) for v in [serial, dst, delta, ]])
